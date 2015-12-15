436407ed02157577b0d33249c0f0c09105b337a5
ragone <ragonedk@gmail.com>
2015-12-15 14:58:16 +1300

    local class

--- src/main/java/aut/model/Exercise.java
+++ src/main/java/aut/model/Exercise.java
@@ -8,7 +8,7 @@ import java.util.UUID;
  * @author ragone.
  * @version 9/12/15
  */
-public class Exercise {
+class Exercise {
     private int reps, sets, restTime;
     private String id, notes;
 
